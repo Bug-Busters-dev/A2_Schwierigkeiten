8ab7a6ac2a5a806a53f72f3edec3e88586b936a3
Bxnni78187 <156448008+Bxnni78187@users.noreply.github.com>
2024-09-02 18:50:10 +0200

    sorter change
    added sorter.rs, the sorter.rs from before is now sorter utils

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+use std::io::{self, Read};
 pub mod reader;
 pub mod sorter;
+pub mod sorter_util;
 const PATH: &str = "./data/schwierigkeiten0.txt";
-fn main() {}
+fn main() {
+    for x in sorter_util::get_klausur_lines_data(0) {
+        print!("{:?}", x)
+    }
+    //println!("{:?}", sorter_util::get_klausur_lines_data(0));
+    end();
+}
+fn end() {
+    println!("Press enter to exit");
+
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).ok(); // ignore the result
+}
