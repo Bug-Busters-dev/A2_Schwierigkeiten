f50da5c4e3800653b3a0f84fa02a188db6d783d2
Bxnni78187 <benni.gbs@gmx.de>
2024-11-10 23:01:16 +0100

    add make_df

--- src/conflicts.rs
+++ src/conflicts.rs
@@ -1,119 +1,140 @@
 use itertools::Itertools;
-// use polars::prelude::*;
-use std::collections::{HashMap, HashSet};
+use polars::prelude::*;
+use std::env;
+use std::vec;
 
 const ABC: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-struct ValuePair {
-    value1: (char, i16),
-    value2: (char, i16),
-}
-fn sort_alphapeticaly(charpair: &str) -> String {
-    let mut chars: Vec<char> = charpair.chars().collect();
-    chars.sort_by(|a, b| a.cmp(b));
-    chars.into_iter().collect<String>()
-}
-
-fn to_struct(charpair: &str, hashmaps: Vec<HashMap<char, u16>>, index: usize) -> ValuePair{
-    
-    for char in charpair.chars() {
-        let value1 = hashmaps[index].get(&char).unwrap();
-        let value2 = hashmaps[index].get(&char).unwrap();
-    }
-
-
-
-}
-
-fn find_conlficts(klausurenvec: Vec<String>, hash_vec: Vec<HashMap<char, u16>>, ) {
-
-    for (i, klausur) in klausurenvec.iter().enumerate() {
-
-
-        for (j, chars) in klausur.chars().enumerate() {
-            hash_vec[i]
-        }
-    }
-
-
-
-
-
-
-    let mut all_pairs: HashSet<String> = HashSet::new();
-    let mut allklaururpairs: Vec<Vec<String>> = Vec::new();
+fn make_df(klassenvec: Vec<String>) -> DataFrame {
+    // make all char pairs for the first m letters into a series
+    /* let m: u32 = sorter_util::get_n_m_k(&path_to_data, 2)
+        .unwrap()
+        .get_value()
+        .unwrap();
+    */
+    env::set_var("POLARS_FMT_MAX_ROWS", "100");
+    let all_pairs: Vec<String> = Vec::new();
+    let mut pair_combinations: Vec<Vec<String>> = Vec::new();
+    let mut klausur_numbers: Vec<u16> = Vec::new();
     // make a series for each line arbeit in klassenvec
     // file has m columns
-    for arbeit in klausurenvec {
-        // Konvertiere den String in ein Set von einzigartigen Buchstaben
-        let unique_chars: HashSet<char> = arbeit.chars().collect();
-        let mut pairs: HashSet<String> = HashSet::new();
+    for (i, arbeit) in klassenvec.iter().enumerate() {
+        let mut pairs: Vec<String> = Vec::new();
         // Erzeuge alle Kombinationen von zwei Buchstaben
-        for (i, (c1, c2)) in unique_chars
-            .iter()
-            .combinations(2)
-            .map(|v| (v[0], v[1]))
-            .enumerate()
-        {
-            // Erstelle die Paare in alphabetischer Reihenfolge
-            let pair = format!("{}{}", c1, c2);
-            all_pairs.insert(pair.clone());
-            // auch in umgekehrter Reihenfolge um AB und BA als gleich zu betrachten
-            let reverse_pair = format!("{}{}", c2, c1);
-            all_pairs.insert(reverse_pair);
-            pairs.insert(pair);
+        for (j, c1) in arbeit.chars().enumerate() {
+            for c2 in arbeit.chars().skip(j + 1) {
+                if c1 != c2 {
+                    let pair = format!("{}{}", c1, c2);
+                    pairs.push(pair);
+                    klausur_numbers.push(i as u16 + 1 as u16);
+                }
+            }
         }
-        let klausurpairs = Vec::from_iter(pairs.clone());
-        allklaururpairs.push(klausurpairs);
-    }
 
-    // make a series for each vec in vecvec
-    for (i, vec) in allklaururpairs.iter().enumerate() {
-        let slice_vec: Vec<&str> = vec.iter().map(|x| x.as_str()).collect();
-        let series = Series::new(format!("Arbeit_{}", i).into(), slice_vec);
-        // create dataframe from series and value series
-        unimplemented!();
-    }
+        let vec = Vec::from_iter(pairs);
 
-    // let max_len = all_series.iter().map(|s| s.len()).max().unwrap_or(0);
-
-    // for series in all_series.iter_mut() {
-  /*   if series.len() < max_len {
-        let pad_count = max_len - series.len();
-        let padding: Vec<&str> = vec![""; pad_count];
-        series.append(&Series::new("null".into(), padding)).unwrap();
+        pair_combinations.push(vec.clone());
     }
-}
-*/
-// make a dataframe from all_series
-/*  let df_pairs = DataFrame::new(
-    all_series
+    let pair_combinations = pair_combinations
+        .into_iter()
+        .flatten()
+        .collect::<Vec<String>>();
+
+    // alphabeticly sorted pair combinations
+    let alpha_sorted_pairs = pair_combinations
+        .clone()
         .iter()
-        .map(|s| s.clone().into())
-        .collect::<Vec<Column>>(),
-)
-.unwrap();
-*/
-// println!("{:?}", df_pairs);
- }
+        .map(|x| x.chars().sorted().collect::<String>())
+        .collect::<Vec<String>>();
+    // let mut all_series: Vec<Series> = Vec::new();
+    // make a series for each vec in vecvec
+    //for (i, vec) in vecvec.iter().enumerate() {
+    //    ljkjjet slice_vec: Vec<&str> = vec.iter().map(|x| x.as_str()).collect();
+    //    let series = Series::new(format!("Arbeit_{}", i).into(), slice_vec);
+    //    all_series.push(series);
+    //}i
+    //let max_len = all_series.iter().map(|s| s.len()).max().unwrap_or(0);
+    //for series in all_series.iter_mut() {
+    //    if series.len() < max_len {
+    //        let pad_count = max_len - series.len();
+    //        let padding: Vec<&str> = vec![""; pad_count];
+    //        series.append(&Series::new("null".into(), padding)).unwrap();
+    //    }
+    //}
+    // make a dataframe from all_series
 
+    let char_series: Series = Series::new("CharPairs".into(), pair_combinations)
+        .cast(&DataType::String)
+        .unwrap();
+    let sorted_series: Series = Series::new("SortedPairs".into(), alpha_sorted_pairs)
+        .cast(&DataType::String)
+        .unwrap();
+    let klausur_series: Series = Series::new(
+        "Klausur".into(),
+        klausur_numbers
+            .iter()
+            .map(|&x| x as i32)
+            .collect::<Vec<i32>>(),
+    )
+    .cast(&DataType::Int32)
+    .unwrap();
+
+    let df_pairs = DataFrame::new(vec![
+        char_series.into_column(),
+        sorted_series.into_column(),
+        klausur_series.into_column(),
+    ])
+    .unwrap();
+    println!("{:?}", df_pairs);
+    df_pairs
+}
+
+fn _locate_conflicts(_dataframe: DataFrame, klassenvec: Vec<String>) {
+    let df = make_df(klassenvec);
+    let pairs = df.get_columns().iter().nth(1).unwrap();
+    let pairs_series: Series = pairs.as_series().unwrap().to_owned();
+
+    let dublicates = df
+        .clone()
+        .lazy()
+        .group_by(["SortedPairs"])
+        .agg(vec![col("SortedPairs").count().alias("count")])
+        .filter(col("count").gt(1))
+        .collect()
+        .unwrap();
+
+    println!("{:?}", dublicates);
+    // make dublicates to strings.
+    /* let chars_pairs_series = dublicates
+        .get_columns()
+        .iter()
+        .nth(0)
+        .unwrap()
+        .as_series()
+        .unwrap()
+        .to_owned();
+    let strings: Vec<String> = chars_pairs_series
+        .str()
+        .unwrap()
+        .into_iter()
+        .filter_map(|opt_str| opt_str.map(|s| s.to_string()))
+        .collect();
+    */
+    /*  for every dublicate we wanna see if the char_series is in the same reinfolge wenn nt dann conflict und user warnen
+     */
+
+    
+
+}
 
 #[cfg(test)]
-mod tests {
 #[test]
-#[ignore]
 fn main() {
     let klassenvec = vec![
         "FACG".to_string(),
-        "BADC".to_string(),
+        "BCA".to_string(),
         "EBA".to_string(),
         "BFA".to_string(),
     ];
-    find_conlficts(klassenvec);
-}
-#[test]
-fn test_sort_alphapeticaly() {
-    assert_eq!(sort_alphapeticaly("BA"), "AB");
-    assert_eq!(sort_alphapeticaly("AB"), "AB"); 
-    }
+    let df = make_df(klassenvec.clone());
+    _locate_conflicts(df, klassenvec);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 // lib.rs
 pub mod conflicts;
-pub mod new_conflicts;
 pub mod reader;
 pub mod sorter;
 pub mod sorter_util;
